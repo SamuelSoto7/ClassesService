782f0065797b3297ea921601701b3b69204bacfc
CarlosJaBR <carjabo12@gmail.com>
2025-04-21 20:23:08 -0500

    feat: edit securityconfig

--- src/main/java/com/classes/classesService/config/SecurityConfig.java
+++ src/main/java/com/classes/classesService/config/SecurityConfig.java
@@ -4,60 +4,51 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz ->
-                        authz
-                                .requestMatchers(
-                                        "/swagger-ui/**",
-                                        "/v3/api-docs/**",
-                                        "/swagger-ui.html",
-                                        "/classes/public/**"
-                                )
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/classes/debug-token").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt ->
-                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
                         )
                 )
-                .csrf(csrf ->
-                        csrf.ignoringRequestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        )
-                )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                .csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
-    private Converter<
-            Jwt,
-            ? extends AbstractAuthenticationToken
-            > jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter =
-                new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(
-                new KeycloakRealmRoleConverter()
-        );
-        return jwtConverter;
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8015/realms/gym-ApiGateway/protocol/openid-connect/certs").build();
+    }
+
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter delegate = new JwtGrantedAuthoritiesConverter();
+        delegate.setAuthoritiesClaimName("realm_access.roles");
+        delegate.setAuthorityPrefix("");
+
+        return jwt -> {
+            Collection<GrantedAuthority> authorities = delegate.convert(jwt);
+            return new JwtAuthenticationToken(jwt, authorities);
+        };
     }
 }
